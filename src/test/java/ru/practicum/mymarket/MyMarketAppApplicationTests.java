f923ac9da41b4d764ace8bb70466def3e2d81782
Eugene Buslov <buslov83@gmail.com>
2026-04-11 19:53:04 +0400

    Init Spring Boot project

---
+++ src/test/java/ru/practicum/mymarket/MyMarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.mymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
