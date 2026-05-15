89888c87b839399553a3e0b3f907890b16c037d5
Eugene Buslov <buslov83@gmail.com>
2026-05-15 16:48:11 +0400

    Migrate data model to Spring Data

---
+++ src/main/java/ru/practicum/mymarket/reactive/model/Order.java
@@ -0,0 +1,36 @@
+package ru.practicum.mymarket.reactive.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.Instant;
+
+@Table("orders")
+public class Order {
+
+    @Id
+    private Long id;
+
+    @Column("created_at")
+    private Instant createdAt;
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
