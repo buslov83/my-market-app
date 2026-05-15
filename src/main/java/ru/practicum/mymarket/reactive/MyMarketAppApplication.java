bc50e356d047162c4eb133d570d1f3d39e105a52
Eugene Buslov <buslov83@gmail.com>
2026-05-15 15:57:14 +0400

    Add Spring WebFlux and R2DBC dependencies

---
+++ src/main/java/ru/practicum/mymarket/reactive/MyMarketAppApplication.java
@@ -0,0 +1,15 @@
+package ru.practicum.mymarket.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Reactive application entry point
+ */
+@SpringBootApplication
+public class MyMarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMarketAppApplication.class, args);
+    }
+}
